ae90839c66943e509b88ec10a2a030cd1b1f4c13
ElenMar4 <mar4enko.elena.jurjevna@gmail.com>
2023-10-29 18:27:57 +0300

    initial commit spring_shell

---
+++ hw_04_spring_shell/src/test/java/ru/otus/marchenko/dao/CsvQuestionDaoTest.java
@@ -0,0 +1,69 @@
+package ru.otus.marchenko.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.marchenko.config.TestFileNameProvider;
+import ru.otus.marchenko.domain.Answer;
+import ru.otus.marchenko.domain.Question;
+import ru.otus.marchenko.exceptions.QuestionReadException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = CsvQuestionDao.class)
+@DisplayName("Класс CsvQuestionDao ")
+public class CsvQuestionDaoTest {
+
+    @MockBean
+    private TestFileNameProvider fileNameProvider;
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    private final List<Question> provideList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        questionDao = new CsvQuestionDao(fileNameProvider);
+        provideList.add(new Question("1= ?", List.of(
+                new Answer("1", true),
+                new Answer("2", false),
+                new Answer("3", false)))
+        );
+        provideList.add(new Question("2= ?", List.of(
+                new Answer("1", false),
+                new Answer("2", true),
+                new Answer("3", false)))
+        );
+        provideList.add(new Question("3= ?", List.of(
+                new Answer("1", false),
+                new Answer("2", false),
+                new Answer("3", true)))
+        );
+    }
+
+    @Test
+    @DisplayName("корректно разбирает данные из CSV-файла и преобразует в список объектов Question")
+    public void parseCSVFileAndCorrectGetListQuestions() {
+        when(fileNameProvider.getTestFileName()).thenReturn("testQuestions.csv");
+        List<Question> parsList = questionDao.findAll();
+        assertThat(provideList.equals(parsList)).isTrue();
+    }
+
+    @Test
+    @DisplayName("выбрасывает исключение типа QuestionReadException, когда файл для чтения не найден")
+    public void methodFindAllThrowsExceptionWhenFileNotFound() {
+        when(fileNameProvider.getTestFileName()).thenReturn("notFindFile.csv");
+        assertThatThrownBy(() -> questionDao.findAll())
+                .isInstanceOf(QuestionReadException.class)
+                .hasMessageContaining("File read error");
+    }
+}
